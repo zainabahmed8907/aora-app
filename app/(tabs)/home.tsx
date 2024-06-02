2ba3dbce57b01bb5a68c68f0ea8540fee03ae8bf
zainabahmed8907 <zaahmedmughal@gmail.com>
2024-06-02 18:59:21 +0500

    some chanegs

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,17 +1,42 @@
-import { View, Text, FlatList, Image } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  RefreshControl,
+  Alert,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "@/constants";
+import SearchField from "@/components/SearchField";
+import Trending from "@/components/Trending";
+import EmptyState from "@/components/EmptyState";
+import { getAllposts } from "@/lib/appwrite";
+import { useAppwrite } from "@/lib/useAppwrite";
+import VideoCard from "@/components/VideoCard";
 
 const Home = () => {
+  const [refershing, setRefreshing] = useState(false);
+  const [data, setData] = useState([]);
+
+  const [isLoading, setLoading] = useState(true);
+  const { data: posts, refetch } = useAppwrite(getAllposts);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={[]}
+        data={posts}
         keyExtractor={(item) => item?.$id}
         renderItem={({ item }) => {
-          return <Text className="text-3xl">{item?.$id}</Text>;
+          return <VideoCard video={item}/>;
         }}
+
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
             <View className="justify-between items-start flex-row mb-6">
@@ -32,8 +57,25 @@ const Home = () => {
                 />
               </View>
             </View>
+
+            <SearchField />
+
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-gray-100 text-lg font-pregular mb-3">
+                Latest Videos
+              </Text>
+
+              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 } ?? []]} />
+            </View>
           </View>
         )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Video found"
+            subtitle="Be the first one to upload the video"
+          />
+        )}
+        refreshControl={<RefreshControl refreshing={refershing} />}
       />
     </SafeAreaView>
   );
--- components/SearchField.tsx
+++ components/SearchField.tsx
@@ -2,38 +2,21 @@ import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 import React, { useState } from "react";
 import { icons } from "@/constants";
 
-const SearchField = ({
-  title,
-  onChangeText,
-  otherStyles,
-  keyboardType,
-  value,
-  placeholder,
-}) => {
+const SearchField = ({ onChangeText, keyboardType, value, placeholder }) => {
   const [showPassword, setshowPassword] = useState(false);
   return (
-    <View className={`space-y-2 ${otherStyles}`}>
-      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
-      <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary flex-row items-center">
-        <TextInput
-          className="flex-1 text-white font-semibold text-base"
-          value={value}
-          placeholder={placeholder}
-          secureTextEntry={title == "Password" && !showPassword}
-          keyboardType={keyboardType}
-          onChangeText={onChangeText}
-          placeholderTextColor="#7B7B8B"
-        />
-        {title == "Password" && (
-          <TouchableOpacity onPress={() => setshowPassword(!showPassword)}>
-            <Image
-              source={!showPassword ? icons.eye : icons.eyeHide}
-              className="w-6 h-6"
-              resizeMode="contain"
-            />
-          </TouchableOpacity>
-        )}
-      </View>
+    <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary flex-row items-center">
+      <TextInput
+        className="text-base mt-0.5 text-white flex-1 font-pregular "
+        value={value}
+        placeholder={placeholder}
+        keyboardType={keyboardType}
+        onChangeText={onChangeText}
+        placeholderTextColor="#7B7B8B"
+      />
+      <TouchableOpacity onPress={() => setshowPassword(!showPassword)}>
+        <Image source={icons.search} className="w-5 h-5" resizeMode="contain" />
+      </TouchableOpacity>
     </View>
   );
 };
